d321ddbeb84cb8db733221fde3debe4fcd31b053
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-11 01:47:53 +0600

    axios for http req installed

--- src/App.js
+++ src/App.js
@@ -8,81 +8,81 @@ import uuid from "uuid";
 import "./App.css";
 
 class App extends Component {
-    state = {
-        todos: [
-            {
-                id: uuid.v4(),
-                title: "Take out the trash",
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: "Dinner with Girlfriend",
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: "Meeting with Boss",
-                completed: false
-            }
-        ]
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: "Dinner with Girlfriend",
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: "Meeting with Boss",
+        completed: false
+      }
+    ]
+  };
+
+  // ?Toggle complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // ? Delete Todo
+
+  delTodo = id => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  };
+
+  //* Add Todo
+
+  addTodo = title => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
     };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
-    // ?Toggle complete
-    markComplete = id => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed;
-                }
-                return todo;
-            })
-        });
-    };
-
-    // ? Delete Todo
-
-    delTodo = id => {
-        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
-    };
-
-    //* Add Todo
-
-    addTodo = title => {
-        const newTodo = {
-            id: uuid.v4(),
-            title,
-            completed: false
-        };
-        this.setState({ todos: [...this.state.todos, newTodo] });
-    };
-
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <div className="container">
-                        <Header />
-                        <Route
-                            exact
-                            path="/"
-                            render={props => (
-                                <React.Fragment>
-                                    <Todos
-                                        todos={this.state.todos}
-                                        markComplete={this.markComplete}
-                                        delTodo={this.delTodo}
-                                    />
-                                    <AddTodo addTodo={this.addTodo} />
-                                </React.Fragment>
-                            )}
-                        />
-                        <Route path='/about' component={About} />
-                    </div>
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                  <AddTodo addTodo={this.addTodo} />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
