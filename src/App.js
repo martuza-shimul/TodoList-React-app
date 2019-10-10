31175109e19d9ac9dc2ed5359784588bebb8ab18
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-11 01:16:29 +0600

    addTodo functionality added

--- src/App.js
+++ src/App.js
@@ -43,6 +43,18 @@ class App extends Component {
     delTodo = id => {
         this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
     };
+
+    //* Add Todo
+
+    addTodo = title => {
+      const newTodo = {
+        id: 4,
+        title,
+        completed: false
+      }
+      this.setState({ todos: [...this.state.todos, newTodo] });
+    }
+
     render() {
         return (
             <div className="App">
@@ -53,7 +65,7 @@ class App extends Component {
                         markComplete={this.markComplete}
                         delTodo={this.delTodo}
                     />
-                    <AddTodo />
+                    <AddTodo addTodo={this.addTodo}/>
                 </div>
             </div>
         );
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,25 +1,30 @@
 import React, { Component } from "react";
 
 export class AddTodo extends Component {
-
     state = {
-        title: ''
-    }
+        title: ""
+    };
 
-    onChange = e => this.setState();
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: '' })
+    }
 
     render() {
         return (
-            <form style={{ display: 'flex'}}>
-                <input 
-                    type="text" 
-                    name="title" 
-                    style={{flex: '10', padding: '5px'}}
-                    placeholder="Add Todo ..." 
-                    value= {this.state.title}
+            <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+                <input
+                    type="text"
+                    name="title"
+                    style={{ flex: "10", padding: "5px" }}
+                    placeholder="Add Todo ..."
+                    value={this.state.title}
                     onChange={this.onChange}
                 />
-                <input type="submit" value="Submit" className="btn" style={{ flex: '1' }}/>
+                <input type="submit" value="Submit" className="btn" style={{ flex: "1" }} />
             </form>
         );
     }
